69f272aea88dd2231398c2faabab3cbfa06f2551
RandyRodriguez <a.ya.miheev@gmail.com>
2018-08-01 14:47:41 +0300

    push to github all I have up to this point: some draft of a html page, bunch of lines with js-client to render rss feed, small python backend to parse and return posts from rss feeds to js-client

---
+++ get_posts_from_feed.py
@@ -0,0 +1,91 @@
+#!python3
+# -*- coding: utf-8 -*-
+# some kind of backend which takes ajax-request, reads certains rss feeds and return specific information from
+# them as json with list of posts
+
+from flask import Flask
+import feedparser
+import json
+from flask import request
+app = Flask(__name__)
+
+rss_links = {'ferra-articles': 'https://www.ferra.ru/export/articles-rss.xml',
+             'techradar': 'https://www.techradar.com/rss',
+             'verge': 'https://www.theverge.com/rss/index.xml',
+             'ubkreview': 'https://www.ultrabookreview.com/feed/',
+             'bi': 'https://www.businessinsider.com/sai/rss?IR=T',
+             'tproger': 'https://tproger.ru/feed/',
+             '3dnews-hard': 'https://3dnews.ru/news/rss/',
+             'techspot': 'https://www.techspot.com/backend.xml'}
+
+
+@app.route('/')
+def start():
+    return 'Hi! This is a small server that parses rss feed and returns some items from it as json'
+
+
+# Function that figures out which rss feed to return, gets it from another function, convert to json and returns
+@app.route('/getfeed')
+def start_parse_rss():
+
+    rss_source = request.args.get('rsource')
+
+    # If you need to return all rss feeds in one
+    if rss_source == 'all':
+        mixed_feed = []
+        for link in rss_links:
+            mixed_feed.extend(parse_rss(link))
+        new_mixed_feed = sorted(mixed_feed, key=lambda k: k['published'])
+        return json.dumps(new_mixed_feed, indent=4, sort_keys=True, separators=(',', ': '), ensure_ascii=False)
+
+    # if you need to return post only of one feed
+    feed = parse_rss(rss_links[rss_source])
+    return json.dumps(feed, indent=4, sort_keys=True, separators=(',', ': '), ensure_ascii=False)
+
+
+def parse_rss(link):
+
+    one_feed = []
+
+    # add some visual highlight
+    print(5 * '\n')
+    line = '*'
+    for i in range(10):
+        print(line)
+        line = ' ' + line
+    print('RSS FEED: ', link)
+    for i in range(10):
+        line = line[1:]
+        print(line)
+
+    rss = feedparser.parse(link)  # Get file from internet, open it with xml-parser
+
+    # print(et.tostring(root, encoding='utf8').decode('utf8'))
+    for entry in rss.entries:
+        post = {}
+        print('Title: ', entry.title)
+        post['title'] = entry.title
+
+        print('Published: ', entry.published)
+        post['published'] = entry.published
+
+        author = entry.get('author', None)
+        if author:
+            print('Author: ', author)
+        post['author'] = author
+
+        post['summary'] = entry.summary
+
+        post['enclosure'] = []
+        for enclosure in entry.enclosures:
+            print('Enclosure: ', enclosure.href)
+            post['enclosure'].append(enclosure.href)
+
+        print('Link: ', entry.link)
+        post['link'] = entry.link
+
+        print('\nNext item\n')
+        one_feed.append(post)
+
+    return one_feed
+
---
+++ news.js
@@ -0,0 +1,59 @@
+// This code is mostly written with significant help and inspiration from Sergey Puzakov,
+// thought he doesn't know it
+
+// $(function() { ... });
+// is just jQuery short-hand for
+//
+// $(document).ready(function() { ... });
+// What it's designed to do (amongst other things) is ensure that your function is called once
+// all the DOM elements of the page are ready to be used.
+
+$(function() {
+    var RSSReader = function() {
+        // this.feedUrl = 'http://node.dev.puzankov.com/rss/data';
+        this.feedUrl = 'http://127.0.0.1:5000/getfeed';
+        this.arcticlesList = $('.articles');
+        this.articleTmpl = $('.article-tmpl');
+        this.init();
+    };
+
+    RSSReader.prototype.init = function() {
+        this.getFeed('techradar');
+    };
+
+    RSSReader.prototype.renderFeed = function(dataList) {
+        var _self = this,
+            listHtml = [];
+        dataList.forEach(function (item) {
+            listHtml.push(_self.renderItem(item));
+        });
+    };
+
+    RSSReader.prototype.onGetData = function(response) {
+        var data = JSON.parse(response);
+        this.renderFeed(data.items);
+    };
+
+    RSSReader.prototype.getFeed = function(feedId) {
+        $.ajax({
+            url: this.feedUrl,
+            data: { rsourse: feedId},
+            method: 'GET',
+            dataType: 'json'
+        }).done(this.onGetData.bind(this)).fail(function(error) {
+                console.log(error);
+            });
+    };
+
+    RSSReader.prototype.renderItem = function(item) {
+        var newItem = this.articleTmpl.clone().removeClass('article-tmpl');
+        newItem.find('.post-heading').html(item.title);
+        newItem.find('.excerpt').html(item.summary);
+        newItem.find('.author').html(item.author);
+        newItem.find('.date').html(moment().format(item.published));
+        newItem.find('.action-button').attr('href', item.link);
+        return newItem;
+    };
+
+    window.rssReader = new RSSReader();
+});
