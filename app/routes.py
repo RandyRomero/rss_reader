b5420867ebb91066627d4cc8091a25c71b4b474a
RandyRodriguez <a.ya.miheev@gmail.com>
2018-10-23 20:33:51 +0300

    add ability to add fresh news before old ones; add logging instead of print statements; document news.js file

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,20 @@
 #!python3
 # -*- coding: utf-8 -*-
-# some kind of backend which takes ajax-request, reads certains rss feeds and return specific information from
+# some kind of backend which takes ajax-request, reads certain rss feeds and return specific information from
 # them as json with list of posts
 
 import json
 import feedparser
 from flask import request, render_template, url_for
 from app import app
-from datetime import datetime
-import dateutil.parser  # pip install python-dateutil // is needed to convert date from iso to datetime object
+import datetime as dt
+import dateutil.parser  # pip install python-dateutil // to convert date from iso to datetime object
 import pytz
 from bs4 import BeautifulSoup as bs
 import re
-from pprint import pprint
 
 # will be changed when I add database with users, because we need to track this time for every user
-last_time_user_gets_his_news = datetime.timestamp(datetime.now())
+last_time_user_gets_his_news = dt.datetime.timestamp(dt.datetime.now())
 
 
 rss_links = {'ferra-articles': 'https://www.ferra.ru/export/articles-rss.xml',
@@ -41,7 +40,7 @@ def get_img_source(htmlstr):
 # Converting time from strings to timestamp
 def get_timestamp(date):
     try:
-        date_temp = datetime.strptime(date, '%a, %d %b %Y %H:%M:%S %z')
+        date_temp = dt.datetime.strptime(date, '%a, %d %b %Y %H:%M:%S %z')
         return date_temp.timestamp()
     except ValueError:
         # Converting time from iso format (like this 2018-08-02T13:32:37-04:00) to timestamp
@@ -56,22 +55,30 @@ def parse_rss(link, mode):
     Function that takes one link to the rss feed and picks up specific data from it, saves each news as a dictionary,
     push these dictionaries to the list
     :param link: link to rss feed
+    :param mode: what to do - parse all feeds to the end or parse only news that appeared since last returning of
+    a feed to client or just count new news
     :return: list with dictionaries
     """
 
     one_feed = []
     news_counter = 0
-    print('Parsing feed: ', link)
+    app.logger.info(f'Parsing feed: {link}')
     rss = feedparser.parse(link)  # Get file from internet, open it with xml-parser
 
     for entry in rss.entries:
-        # print('news_counter:', news_counter)
         if mode == 'count':
             if get_timestamp(entry.published) < last_time_user_gets_his_news:
                 return news_counter
             else:
                 news_counter += 1
                 continue
+
+        if mode == 'latest':
+            news_item_date = get_timestamp(entry.published)
+
+            # Stop reading RSS if current news is already older than time when user last got the news feed
+            if news_item_date < last_time_user_gets_his_news:
+                return one_feed
 
         post = {'title': entry.title,
                 'published': get_timestamp(entry.published)}
@@ -91,23 +98,32 @@ def parse_rss(link, mode):
 
         one_feed.append(post)
 
-    return one_feed
+    if mode != 'latest':
+        return one_feed
+    else:
+        print('There are no new news at all.')
+        return []
 
 
-def make_big_feed():
-    """ Construct feed with all news from all rss-feeds sorted by time of publishing, but return only N news at once """
+def make_news_feed(mode):
+    """
+    :return: list of news sorted by date of publishing
+    """
     mixed_feed = []
 
     # Put all news together in one list
     for link in rss_links.values():
-        mixed_feed.extend(parse_rss(link, 'get_news'))
+        mixed_feed.extend(parse_rss(link, mode))
 
     # Sort news posts by time of publishing
-    new_mixed_feed = sorted(mixed_feed, reverse=True, key=lambda k: k['published'])
+    return sorted(mixed_feed, reverse=True, key=lambda k: k['published'])
 
+
+def get_feed_generator():
+    new_mixed_feed = make_news_feed('parse_all')
     # Return only some part of news at once
     for x in range(0, len(new_mixed_feed), app.config['NEWS_TO_RETURN_AT_ONCE']):
-        print('news counter', x)
+        app.logger.debug(f'There are {x+app.config["NEWS_TO_RETURN_AT_ONCE"]} news on the page now')
         yield new_mixed_feed[x:x+app.config['NEWS_TO_RETURN_AT_ONCE']]
 
 
@@ -128,7 +144,7 @@ def get_big_feed():
             except StopIteration:
                 return '', 204
         else:
-            big_feed_gen = make_big_feed()
+            big_feed_gen = get_feed_generator()
             return json.dumps(next(big_feed_gen), indent=4, sort_keys=True, separators=(',', ': '), ensure_ascii=False)
     return nested_return_feed
 
@@ -144,25 +160,24 @@ def start():
 
 # Function that figures out which rss feed to return, gets it from another function, convert to json and returns
 @app.route('/get-feed')
-def start_parse_rss():
+def get_all_news():
     global last_time_user_gets_his_news
     global get_big_feed_closure
     # get argument from request where coded what rss_feed page asks from server
     rss_source = str(request.args.get('rsource'))
-    add_news = int(request.args.get('addNews'))
-    print('add news:', add_news)
-    print('rss_source:', rss_source)
+    add_news = request.args.get('addNews')
 
     # If you need to return all rss feeds in one
     if rss_source == 'all':
-        if add_news:
-            print('adding news')
+        app.logger.debug(f'add news: {add_news}')
+        if add_news == 'add old news':
+            app.logger.debug('adding news')
             return get_big_feed_closure()
-        else:
-            print('making new feed')
+        elif add_news == 'refresh news':
+            app.logger.debug('making new feed')
             # Resetting closure to get refreshed feed
             get_big_feed_closure = get_big_feed()
-            last_time_user_gets_his_news = datetime.timestamp(datetime.now())
+            last_time_user_gets_his_news = dt.datetime.timestamp(dt.datetime.now())
             return get_big_feed_closure()
     # if you need to return posts only from one particular source
     feed = parse_rss(rss_links[rss_source], 'get_news')
@@ -171,8 +186,9 @@ def start_parse_rss():
 
 @app.route('/fresh-news-counter')
 def get_news_counter():
-    """Launch parsing of rss feeds to count number of news that have
-    appeared since last time server returned news to this user"""
+    """
+    :return: number of news that have appeared since last time server returned news to this user
+    """
     counter = 0
     for link in rss_links.values():
         news_number = parse_rss(link, 'count')
@@ -183,3 +199,13 @@ def get_news_counter():
 
     return str(counter)
 
+
+@app.route('/get-latest-news')
+def get_latest_news():
+    """
+    :return: list of news that have appeared since user get news last time
+    """
+    global last_time_user_gets_his_news
+    latest_news = make_news_feed('latest')
+    last_time_user_gets_his_news = dt.datetime.timestamp(dt.datetime.now())
+    return json.dumps(latest_news, indent=4, sort_keys=True, separators=(',', ': '), ensure_ascii=False)
--- app/static/js/news.js
+++ app/static/js/news.js
@@ -1,10 +1,12 @@
-// This code is mostly written with significant help and inspiration from Sergey Puzakov,
+// This code is mostly written with significant help and inspiration by Sergey Puzakov,
 // thought he doesn't know it
 
+
+// todo make function that updates the timestamp on every news on a page once in a minute
+
+
 // $(function() { ... });
-// is just jQuery short-hand for
-//
-// $(document).ready(function() { ... });
+// is just jQuery short-hand for $(document).ready(function() { ... });
 // What it's designed to do (amongst other things) is ensure that your function is called once
 // all the DOM elements of the page are ready to be used.
 
@@ -13,78 +15,112 @@ $(function() {
         this.feedUrl = 'http://127.0.0.1:5000/get-feed';
         this.articlesList = $('.articles');
         this.articleTmpl = $('.article-tmpl');
+        this.freshNewsButton = $('.fresh-news-counter');
         this.init();
     };
 
     RSSReader.prototype.checkUpdates = function() {
-        // Recursively checking for more news
+        /**
+         * Recursively checking for more news
+         */
         setTimeout(() => {
-            this.getNewsCount();
+            this.getNewsCounter();
             this.checkUpdates();
-        } , 30000);
+        } , 60000);
     };
-
 
     RSSReader.prototype.init = function() {
-        this.getFeed('all', 0);
-        this.checkUpdates();
+        /* Things to do right after initialization */
+        this.getFeed('all', 'refresh news');
+        this.freshNewsButton.on('click', this.getLatestNews.bind(this));
     };
 
-    // Function that tracks if user at the and of a page. If so, it call function to render new news
-    // and turn off the handler which activates this trackScroll function
-
     RSSReader.prototype.trackScroll = function() {
+        /*
+            It tracks whether user at the end of a page. If so, it calls function to render new news
+            and turn off the handler which activates this trackScroll function
+         */
+
         let heightOfPage = document.documentElement.scrollHeight;
         let currentScroll = pageYOffset;
         let heightOfWindow = document.documentElement.clientHeight;
-        console.log(`currentScroll: ${currentScroll}, heightOfWindow: ${heightOfWindow}, heightOfPage ${heightOfPage}`);
-        console.log((currentScroll + heightOfWindow) >= (heightOfPage - 200));
+        // console.log(`currentScroll: ${currentScroll}, heightOfWindow: ${heightOfWindow}, heightOfPage ${heightOfPage}`);
+        // console.log((currentScroll + heightOfWindow) >= (heightOfPage - 200));
         if ((currentScroll + heightOfWindow) >= (heightOfPage - 200)) {
-            console.log('adding more news');
-            console.dir(this);
+            console.log('adding more news as reached and of the page');
+
+            // The .off() method removes event handlers that were attached with .on().
             $(window).off('scroll', this.scrollListener);
-            this.getFeed('all', 1);
+            this.getFeed('all', 'add old news');
         }
     };
 
-    RSSReader.prototype.renderFeed = function(dataList) {
+    RSSReader.prototype.renderFeed = function(dataList, mode) {
+        /*
+        For every item in a given list it calls function to render the item with html template
+
+         @param {array} dataList - list of items where each item is an object with key-value pairs that
+         comprises one news
+         @param {str} note - how to add news on a page - before already rendered news or after
+         @return none
+         */
         let _self = this,
             listHtml = [];
         dataList.forEach(function (item) {
             listHtml.push(_self.renderItem(item));
         });
 
-        this.articlesList.append(listHtml);
+        if (mode === 'append') {
+            console.log('Rendering the whole page (or add older news as user scrolls down the page)');
+            this.articlesList.append(listHtml);
+        }
+        else if (mode === 'prepend') {
+            console.log('Just adding the latest news above old ones');
+            this.articlesList.prepend(listHtml);
+        }
     };
 
-    RSSReader.prototype.onGetData = function(response) {
-        this.renderFeed(response);
-    };
 
     RSSReader.prototype.getFeed = function(feedId, addNews) {
-        // feedId - name of the resource to pick the right link to rss feed
-        // addNews - if it is 1 - we ask server to send next portion of news that already were parsed by server
-        // if it is 0 - we ask server to parse feed(s) again and send a new news feed
-        console.log('Sending ajax-request to get more news...');
-        console.log('adding news:', addNews);
+        /*
+        @param {str} feedId - name of the resource to pick the right link to rss feed
+        @param {str} addNews - mode of adding news: refresh all page or just add old news after already
+         rendered news (infinite feed)
+        */
+
+        if (addNews === 'refresh news') {
+            console.log('Sending ajax-request to parse all rss feeds and get a new news feed...');
+        }
+        else if (addNews === 'add old news') {
+            console.log('Sending ajax-request to get next portion of news as user scrolls the page...')
+        }
         $.ajax({
             url: this.feedUrl,
             data: { rsource: feedId, addNews: addNews},
             method: 'GET',
             dataType: 'json'
         }).done((response) => {
-            this.onGetData(response);
+            this.renderFeed(response, 'append');
             this.scrollListener = this.trackScroll.bind(this);
             $(window).on('scroll', this.scrollListener);
             this.articlesList.addClass('articles-loaded');
+            this.checkUpdates();
         })
             .fail((error) => { console.log(error);});
     };
 
     RSSReader.prototype.renderItem = function(item) {
+        /*
+        Fills in HTML-template with data from server - one news. Specifically with a heading of that news,
+        with name of a source, author name, date of publishing, link to image and link to website where that
+        news was published
+        @param {object} item - object that comprise one news and contains data of publishing and all that stuff
+        to be rendered as one news item on a page
+        return {str} newItem - string which is html-code ready to be added on a page
+         */
+
         let newItem = this.articleTmpl.clone().removeClass('article-tmpl');
         newItem.find('.post-heading').html(item.title);
-        // newItem.find('.excerpt').html(item.summary);
         if (item['domain_name']) {
             newItem.find('.author').html(' by ' + item['domain_name']);
         }
@@ -100,24 +136,49 @@ $(function() {
         return newItem;
     };
 
-    RSSReader.prototype.renderMoreNewsButton = function(newsCounter) {
-        // show big button above news feed with counter how much news have appeared in rss feed since user updated the
-        // page last time
+    RSSReader.prototype.getLatestNews = function() {
+        /*
+        Gets from server list of news that have appeared since user got news last time
+        In case ajax request was successive the function calls render function to add the lastest news to the page
+        and hides button that activates this function
+         */
 
-        if (newsCounter < 1) {
-            console.log('There are no new news.');
-            return;
-        }
-        let freshNewsButton = $('.fresh-news-counter');
-        freshNewsButton.removeClass('tmpl'); // make element visible
-
-        let counter = $('.fresh-news-counter h2');
-        counter.text('More news: ' + newsCounter);
+        console.log('Sending request for the latest news.');
+        $.ajax({
+            url: 'http://127.0.0.1:5000/get-latest-news',
+            method: 'GET',
+            dataType: 'json'
+        }).done((response) => {
+            console.log('We got latest news!');
+            console.dir(response);
+            this.renderFeed(response, 'prepend');
+            this.freshNewsButton.addClass('tmpl');
+            console.log('Hiding fresh news button');
+        }).fail((error) => { console.log(error); })
     };
 
-    RSSReader.prototype.getNewsCount = function() {
-        // Make ajax-request to get quantity of news have appeared in rss feed since user updated the
-        // page last time
+    RSSReader.prototype.renderMoreNewsButton = function(newsCounter) {
+        /*
+        shows big button above news feed with the counter how much news have appeared in rss feed since user updated the
+        page last time
+        @param {integer} newsCounter - number of fresh news. This integer will be shown on button above news
+         */
+
+        if (this.freshNewsButton.hasClass('tmpl')) {
+            this.freshNewsButton.removeClass('tmpl'); // make element visible
+            this.articlesList.prepend(this.freshNewsButton); // move button upon news feed
+            return;
+        }
+
+        $('.fresh-news-counter h2').text('More news: ' + newsCounter);
+    };
+
+
+    RSSReader.prototype.getNewsCounter = function() {
+        /*
+            Make ajax-request to get quantity of news that have appeared in rss feed since user updated the
+            page last time
+         */
 
         console.log('Sending ajax-request to get number of fresh news...');
         $.ajax({
@@ -125,7 +186,11 @@ $(function() {
             method: 'GET',
             dataType: 'json'
         }).done((response) => {
-            console.log('We\'ve got the counter!');
+            console.log('Server responded with the counter');
+            if (response < 1) {
+               console.log('There are no new news.');
+                return;
+            }
             this.renderMoreNewsButton(response);
         }).fail((error) => { console.log(error); })
     };
