9d5dedf64ce49932dce68d0ddcf93829af2fb9d1
RandyRodriguez <a.ya.miheev@gmail.com>
2018-10-23 20:06:58 +0300

    Import logging module and change default app.logger formating

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,30 @@
-from flask import Flask, request
+import logging
+from flask import Flask
 from .config import Config
-import os
+from logging.config import dictConfig
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '%(levelname)s %(asctime)s %(module)s LINE %(lineno)d: %(message)s',
+        'datefmt': '%H:%M:%S'
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
+    }
+})
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.static_folder = 'static'
+
 
 from app import routes
